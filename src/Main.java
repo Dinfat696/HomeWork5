5dc6415f576c478151e6a63222f8cf9b2250463e
dinarfathrahmanov <dinfat@mail.ru>
2023-02-09 21:41:07 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+
+
+    }
+
+    public static void task1(){
+        System.out.println("Задача 1");
+        char clientOS = 0;
+        if (clientOS == 0){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 }
