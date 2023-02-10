8219b427c057ee570c7eaff47a0ac425dbe5d65a
dinarfathrahmanov <dinfat@mail.ru>
2023-02-10 20:39:02 +0300

    задача 3

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
 
 
     }
@@ -37,4 +38,16 @@ public class Main {
             System.out.println("Установите  обычное приложение");
         }
     }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int year = 2020;
+        if ((year % 4 == 0&&year%100!=0) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
 }
+
+
